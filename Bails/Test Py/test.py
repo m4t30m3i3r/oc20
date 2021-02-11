f97f261f197af10224c4533219049980e7d6fbf8
m4t30m3i3r <77661971+m4t30m3i3r@users.noreply.github.com>
2021-02-11 15:20:16 +0100

    ldfhgj

--- Bails/Test Py/test.py
+++ Bails/Test Py/test.py
@@ -1,24 +1,72 @@
 import pygame
 pygame.init()
 
-
+#variables couleurs
 White = (255, 255, 255)
 Red = (255, 0 , 0)
 Green = (0, 255, 0)
 Blue = (0, 0, 255)
+Black = (0, 0, 0)
 
+#crée l'écran
 screen = pygame.display.set_mode([800, 600])
-pygame.display.set_caption("Snoop Dogg")
-icon = pygame.image.load('joint.png')
+
+#Titre et icone
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('cosmos.jpg')
 pygame.display.set_icon(icon)
 
+#joueur
+playerIMG = pygame.image.load('ally.png')
+playerX =  370
+playerY = 480
+playerX_change = 0
+
+
+#enemy
+enemyIMG = pygame.image.load('enemy.png')
+enemyX = 370
+enemyY = 30
+enemyX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerIMG, (x, y))
+    
+def enemy(x,y):
+    screen.blit(enemyIMG, (x, y))
+
+#boucle jeu
 running = True
 while running:
+    
+    screen.fill(Black)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # pressage de touche
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = +2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+                
+                
+    playerX += playerX_change
     
-    pygame.display.flip()
-    screen.fill(Blue)
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
+
+#   pygame.display.flip()
 
 pygame.quit()
---
+++ editor/README.md
@@ -0,0 +1,30 @@
+# oc20 - projet Editor
+
+## Editor
+
+- éditeur graphique
+- permet de manipuler des objets (rectangles, cercles, lignes, images)
+- permet de choisir couleurs et épaisseurs
+- permet de créer, déplacer, supprimer des objets
+- permet de sauvgarder/charger un dessin
+
+## Description
+Décrivez ce que votre éditeur peut faire
+
+## Interface
+
+Capture d'écran
+
+## Raccourcis claviers
+
+- R Pour insérer un rectangle
+- L pour insérer une ligne
+- c pour insérer un cercle
+
+- alt+1 pour ligne d'épaisseur 1
+- alt+0 pour une figure remplie
+- alt+2 pour ligne d'épaisseur 2
+
+## Manipulation 
+
+## Conclusion
